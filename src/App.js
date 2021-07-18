25331e71d63f56bb7e619d4c3ec679c0d3cede96
ksu <ksu.zhytomirsky@gmail.com>
2021-07-18 15:33:21 +0200

    to recheck

--- src/App.js
+++ src/App.js
@@ -1,85 +1,9 @@
 import React from "react";
 import './App.css';
-import * as roomsConstants from './dictionary/roomsConstants';
-import * as bookingInfo from './dictionary/bookingInfoForm';
-import firebase from 'firebase';
 
-class Rooms extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            tableFormsState: Array(6).fill(false),
-            selectedTable: 0,
-            name: '',
-            phone: '',
-            startTimeOfBooking: '',
-            endTimeOfBooking: '',
-        };
-    }
-
-    handleChange = ({target: {value, id}}) => {
-        this.setState({
-            [id]: value,
-        })
-    }
-
-    handleSubmit = () => {
-        const {name, phone, startTimeOfBooking, endTimeOfBooking} = this.state;
-        const db = firebase.database();
-        db.ref(name).push(phone);
-        db.ref(name).push(startTimeOfBooking, endTimeOfBooking);
-}
-
-    handleClick = (id) => {
-        this.setState({selectedTable: id});
-        let buttons = this.state.tableFormsState.slice();
-        buttons[id-1] = !buttons[id-1];
-        this.setState({tableFormsState: buttons});
-    }
-
-    MakeButton = (id) => {
-        return (
-            <div>
-                <input type="button" value={roomsConstants.DICT_ROOMS[id-1].title} onClick={() => this.handleClick(id)}/>
-                <bookingInfo.Form open={this.state.tableFormsState[id-1]}/>
-            </div>
-        );
-    }
-
-    render(){
-        console.log(this.state);
-        return(
-            <div>
-                <div className="room1">
-                    <h5>Room 1</h5>
-                    <div className="room1-button-row">
-                        {this.MakeButton(1)}
-                        {this.MakeButton(2)}
-                    </div>
-                    <div className="room1-button-row">
-                        {this.MakeButton(3)}
-                        {this.MakeButton(4)}
-                    </div>
-                </div>
-                <div className="room2">
-                    <h5>Room 2</h5>
-                    {this.MakeButton(5)}
-                </div>
-                <div className="conferenceRoom">
-                    <h5>Conference Room</h5>
-                    {this.MakeButton(6)}
-                </div>
-            </div>
-        );
-    }
-}
+import {Rooms} from "./elements/Rooms";
 
 class App extends React.Component{
-    componentDidMount() {
-        const db = firebase.database();
-        console.log(db)
-    }
-
     render() {
         return (
             <div className="App">
---
+++ src/elements/RoomBookingForm.js
@@ -0,0 +1,46 @@
+default export class RoomBookingForm extends React.PureComponent{
+    render() {
+        if (!this.props.open) {
+            return null
+        }
+        const {handleChange, handleSubmit} = this.props
+            return (
+                <div>
+                    <form className="forms" onSubmit={handleSubmit}>
+                        <label>
+                            Name:
+                            <input
+                                type="text"
+                                id="name"
+                                onChange={handleChange}/>
+                        </label>
+                        <label>
+                            Phone:
+                            <input
+                                type="tel"
+                                id="phone"
+                                onChange={handleChange}/>
+                        </label>
+                        <label>
+                            Start time of booking:
+                            <input
+                                type="datetime-local"
+                                id="startTimeOfBooking"
+                                onChange={handleChange}/>
+                        </label>
+                        <label>
+                            End time of booking:
+                            <input
+                                type="datetime-local"
+                                id="endTimeOfBooking"
+                                onChange={handleChange}/>
+                        </label>
+                        <input
+                            type="submit"
+                            value="Send"/>
+                    </form>
+                </div>
+            );
+
+    }
+}
